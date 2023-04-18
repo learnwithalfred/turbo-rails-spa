fd9d4b94dd52017558adc8a7ccb5cd39c299758c
Alfred <kwadwo.boateng.alfred@gmail.com>
2023-04-18 06:28:03 +0000

    ye

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -65,6 +65,8 @@ class ArticlesController < ApplicationController
       @article = Article.find(params[:id])
     end
 
+
+
     # Only allow a list of trusted parameters through.
     def article_params
       params.require(:article).permit(:title)
